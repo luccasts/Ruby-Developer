452858c43928f68a15cdad8ba16fda4c92f7ddf0
Lucas Teixeira <politosuldois@gmail.com>
2023-04-27 09:54:54 -0300

    adição do primeiro desafio

---
+++ desafio1.rb
@@ -0,0 +1,13 @@
+print("Informe o seu nome:")
+
+name = gets.chomp()
+
+print("Informe o seu sobrenome:")
+
+sobrenome = gets.chomp()
+
+print("Informe a sua idade:")
+
+idade = gets.chomp()
+
+print ("Bem vindo #{name} #{sobrenome}, #{idade} anos. ")
