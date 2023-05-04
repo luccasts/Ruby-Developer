91d115238aa0024aa8c079a3c1f9430f0652c5e0
Lucas Teixeira <politosuldois@gmail.com>
2023-05-04 19:08:04 -0300

    adições de funções.

--- desafio6.rb
+++ desafio6.rb
@@ -1,10 +1,18 @@
-def HandleWhatsapp (number)
-    phoneSample = /\((\d{2})\)\s+(\d{3})-(\d{4})/.match(number)
-    print(phoneSample)
-    print(1)
+def handleValidate(phoneNumber)
+    phoneSample = /\((\d{2})\)\s+(\d{4})-(\d{4})/.match(phoneNumber)
+    if phoneSample
+        print("Seu número do Whatsapp > #{phoneSample}")
+    else
+        puts("Numero inválido!Informe junto com as expressões desse exemplo: (99) 9999-9999 ")
+        handleValidate(handlePhoneCapture())
+    end
+   
 end 
 
-print ("Informe seu número: ")
-string = "(56) 9999-9999"
-# gets.chomp()
-# HandleWhatsapp(number)
+def handlePhoneCapture
+    print("Informe seu um número > ")
+    phoneNumber = gets.chomp.to_s()
+    return phoneNumber
+end 
+
+handleValidate(handlePhoneCapture())
